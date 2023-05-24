dd2c89edb88d80757b80906759bd07c62ef2c1d6
mac <130864262+IhorVoitiuk@users.noreply.github.com>
2023-05-24 13:47:34 +0300

    added 404.html

--- pastyshop/mainapp/views.py
+++ pastyshop/mainapp/views.py
@@ -14,3 +14,16 @@ def main_page(request):
     products = Product.objects.filter(available=True)[:4]
 
     return render(request, "mainapp/main.html", {"products": products})
+
+
+def custom_404(request, exception):
+    """
+    The custom_404 function is a custom 404 error handler.
+    It takes two arguments: request and exception.
+    The request argument is an HttpRequest object, which contains metadata about the requested page. The exception argument is an Exception object, which contains information about why the 404 error was triggered.
+    
+    :param request: Get the request object, which contains information about the current web request that has triggered this view
+    :param exception: Pass the exception that was raised
+    :return: The 404
+    """
+    return render(request, "mainapp/404.html")
--- pastyshop/pastyshop/urls.py
+++ pastyshop/pastyshop/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
+from django.conf.urls import handler404
+
+# from mainapp import views
 
 from paymentapp import webhooks
 
@@ -40,6 +43,8 @@ urlpatterns = i18n_patterns(
 urlpatterns += [path('payment/webhook/', webhooks.stripe_webhook,
                     name='stripe-webhook'),
 ]
+
+# handler404 = views.custom_404
 
 if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
